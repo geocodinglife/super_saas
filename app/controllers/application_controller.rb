40242dd6febdbc64794a84d1cab466afb972f465
geocodinglife <geocodinglife@gmail.com>
2023-07-12 18:25:44 -0500

    acts_as_tenant add it

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  set_current_tenant_through_filter #acts_as_tenant
+  before_action :set_tenant
+
+   
+  def set_tenant
+    current_tenant = Tenant.second
+    set_current_tenant(current_tenant)
+  end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -3,4 +3,5 @@ class Member < ApplicationRecord
   belongs_to :tenant
 
   validates :user_id, uniqueness: { scope: :tenant_id }
+  acts_as_tenant(:tenant)
 end
